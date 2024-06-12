a382a6947c23d6bf8cbda903718f4cdd0969b8ce
WilAzDev <jhonazcaratework@gmail.com>
2024-06-12 09:24:46 -0500

    Uso de ngfor y ngfor para objetos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,51 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  tasks = signal([
-    'Instalar el proyecto',
-    'Crear proyecto',
-    'Crear componente',
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Instalar angular',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: 'Crear proyecto',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: 'Crear componente',
+      completed: false,
+    },
   ]);
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(title:string){
+    const newTask:Task = {
+      id:Date.now(),
+      title:title,
+      completed: false
+    }
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) =>
+      tasks.filter((task, position) => index !== position)
+    );
+  }
 }
