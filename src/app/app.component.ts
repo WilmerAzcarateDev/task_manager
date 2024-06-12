21c855dcd6a8ace521a11cd158d193ca9e19fc59
WilAzDev <jhonazcaratework@gmail.com>
2024-06-11 20:48:59 -0500

    01: Bienvenido a angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'task_manager';
+  welcome = "Hola !!";
+
+  tasks = [
+    "Instalar el proyecto",
+    "Crear proyecto",
+    "Crear componente"
+  ];
 }
